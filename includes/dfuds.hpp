813c3bf070b9e7ef189432a12df975c69fe6deb7
mati <matilde.rivas@ug.uchile.cl>
2021-07-06 20:34:40 -0400

    -Adds todos for DFUFS

---
+++ includes/dfuds.hpp
@@ -0,0 +1,37 @@
+//
+// Created by anouk on 12-06-21.
+//
+
+#ifndef RANKED_QDAGS_DFUDS_HPP
+#define RANKED_QDAGS_DFUDS_HPP
+
+
+#include <sdsl/bit_vectors.hpp>
+#include <sdsl/bp_support.hpp>
+
+using namespace sdsl;
+using namespace std;
+
+class dfuds
+{
+public:
+
+private:
+
+    bp_support_sada sequence;
+
+protected:
+
+
+
+public:
+
+    dfuds() = default;
+
+    //TODO: nchild
+    //TODO: child_i
+};
+
+#endif //RANKED_QDAGS_DFUDS_HPP
+
+
