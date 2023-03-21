c01c357e2a873bd8ac17b73b7e22d30ad194bbe8
mati <matilde.rivas@ug.uchile.cl>
2023-03-20 22:37:52 -0300

    Test case for 3 relations

--- queries/join_test.cpp
+++ queries/join_test.cpp
@@ -15,6 +15,7 @@ duration<double> time_span_select;
 #define AT_X1 0
 #define AT_X2 1
 #define AT_X3 2
+#define AT_X4 3
 
 
 std::vector<std::vector<uint64_t>>* read_relation(const std::string filename, uint16_t n_Atts)
@@ -61,18 +62,22 @@ int main(int argc, char** argv)
 {
     qdag::att_set att_R;
     qdag::att_set att_S;
+    qdag::att_set att_T;
 
     att_R.push_back(AT_X1); att_R.push_back(AT_X2);
     att_S.push_back(AT_X2); att_S.push_back(AT_X3);
+    att_T.push_back(AT_X3); att_T.push_back(AT_X4);
 
-    std::string strRel_R(argv[1]), strRel_S(argv[2]);
+    std::string strRel_R(argv[1]), strRel_S(argv[2]), strRel_T(argv[3]);
     std::vector<std::vector<uint64_t>>* rel_R = read_relation(strRel_R, att_R.size());
     std::vector<std::vector<uint64_t>>* rel_S = read_relation(strRel_S, att_S.size());
+    std::vector<std::vector<uint64_t>>* rel_T = read_relation(strRel_T, att_T.size());
 
     uint64_t grid_side = 32;
 
     grid_side = maximum_in_table(*rel_R, att_R.size(), grid_side);
     grid_side = maximum_in_table(*rel_S, att_S.size(), grid_side);
+    grid_side = maximum_in_table(*rel_T, att_T.size(), grid_side);
 
     grid_side++;
 
@@ -80,34 +85,31 @@ int main(int argc, char** argv)
 
     qdag qdag_rel_R(*rel_R, att_R, grid_side, 2, att_R.size());
     qdag qdag_rel_S(*rel_S, att_S, grid_side, 2, att_S.size());
+    qdag qdag_rel_T(*rel_T, att_T, grid_side, 2, att_T.size());
 
     std::ostream &output_stream = cout;
     cout << "izquierda:\n";
     qdag_rel_R.print(output_stream);
     cout << "\nderecha:\n";
     qdag_rel_S.print(output_stream);
+    qdag_rel_T.print(output_stream);
 
 //1011011 01100010 00000100 01100110
 //0011011 11000001 01000100 01100110
 //0001001 11100101 01110100 01100110
-    vector<qdag> Q(2);
+    vector<qdag> Q(3);
 
     Q[0] = qdag_rel_R;
     Q[1] = qdag_rel_S;
-    //cout << qdag_rel_R.materialize_node_3_lastlevel(5,1);
-//0100, 0011, 0101, 0010 0100, 0001 1001
-    //bit_vector iactive[] = {{0,1,0,0}, {0,0,1,1}, {0,1,0,1}, {0,0,1,0,0,1,0,0}, {0,0,0,1,1,0,0,1}};   
-    //vector<uint64_t> iactive[]={{1}, {2,3}, {1,3}, {2,5}, {3,4,7}};
-    //Q[0].Q->set_active(iactive);
-    //cout << "\nnuevo active \n";
-    //Q[0].print_active(output_stream);
-    //qdag *Join_Result;
-    //Join_Result = multiJoin(Q, false, 1000);
-    //cout << "\n result:\n";
-    //Join_Result->print(output_stream);
+    Q[2] = qdag_rel_T;
+
+    qdag *Join_Result;
+    Join_Result = multiJoin(Q, false, 1000);
+    cout << "\n result:\n";
+    Join_Result->print(output_stream);
     semiJoin(Q, false, 1000);
     cout << "\n active:\n";
-    //Q[0].print_active(output_stream);
-    cout << endl;
+    Q[0].print_active(output_stream);
+    //cout << endl;
     return 0;
 }
